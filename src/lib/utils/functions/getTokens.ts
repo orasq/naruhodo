6f631ceac79f8881335f9be53237ae5747a487f9
Olivier Rasquin <rasquin.olivier@gmail.com>
2024-09-09 17:05:42 +0200

    fix: add fix for Kuromoji dict URLs issue

--- src/lib/utils/functions/getTokens.ts
+++ src/lib/utils/functions/getTokens.ts
@@ -1,10 +1,14 @@
 import { BatchItem } from "@/hooks/useParseText";
 import { getTokenizer, tokenize } from "kuromojin";
 
+const DICT_PATH = "https://cdn.jsdelivr.net/npm/kuromoji@0.1.2/dict";
+
 export async function getTokens(paragraphs: BatchItem[]) {
   window.kuromojin = {
-    dicPath: "https://cdn.jsdelivr.net/npm/kuromoji@0.1.2/dict",
+    dicPath: DICT_PATH,
   };
+
+  applyKuromojiFix();
 
   await getTokenizer();
 
@@ -19,7 +23,33 @@ export async function getTokens(paragraphs: BatchItem[]) {
     }),
   );
 
-  console.log({ paragraphs, parsedParagraphs });
-
   return parsedParagraphs;
+}
+
+/**
+ * Fix an issue with kuromoji when loading dict from external urls
+ * See: https://github.com/takuyaa/kuromoji.js/issues/37
+ * Adapted from: https://github.com/mobilusoss/textlint-browser-runner/pull/7
+ */
+
+type ExtendedXMLHttpRequest = XMLHttpRequest & {
+  patched_open: (method: string, url: string) => void;
+};
+
+const DICT_PATH_WITHOUT_DOUBLE_SLASH =
+  "https:/cdn.jsdelivr.net/npm/kuromoji@0.1.2/dict";
+
+export function applyKuromojiFix(): void {
+  (XMLHttpRequest.prototype as ExtendedXMLHttpRequest).patched_open =
+    XMLHttpRequest.prototype.open;
+  XMLHttpRequest.prototype.open = function (method: string, url: string) {
+    if (url.indexOf(DICT_PATH_WITHOUT_DOUBLE_SLASH) === 0) {
+      (this as ExtendedXMLHttpRequest).patched_open(
+        method,
+        url.replace(DICT_PATH_WITHOUT_DOUBLE_SLASH, DICT_PATH),
+      );
+    } else {
+      (this as ExtendedXMLHttpRequest).patched_open(method, url);
+    }
+  };
 }
