1307e4c01989852925e5687da4164a856c1fd756
oli <oli@SB-olivierrasquin.local>
2025-02-22 17:41:57 +0100

    chore: clean up ignored files

--- .content-collections/generated/index.d.ts
+++
@@ -1,7 +0,0 @@
-import configuration from "../../content-collections.ts";
-import { GetTypeByName } from "@content-collections/core";
-
-export type Book = GetTypeByName<typeof configuration, "books">;
-export declare const allBooks: Array<Book>;
-
-export {};
